a842b5efdd0ca42259b692b50a6264980ffd7a89
Sergey <krivolapovs@mail.ru>
2020-07-18 08:32:57 +0300

    Условие: Создайте проект по шаблону Console Application. Требуется: Создать класс с именем Rectangle. В теле класса создать два поля, описывающие длины сторон double side1, side2. Создать пользовательский конструктор Rectangle(double side1, double side2), в теле которого поля side1 и side2 инициализируются значениями аргументов. Создать два метода, вычисляющие площадь прямоугольника - double AreaCalculator() и периметр прямоугольника - double PerimeterCalculator(). Создать два свойства double Area и double Perimeter с одним методом доступа get. Написать программу, которая принимает от пользователя длины двух сторон прямоугольника и выводит на экран периметр и площадь.
    Сделано: сделано все по условию, добавлены комментарии
    
    Узнать: почему не работает конструкция под маркером undone

--- KSergey.HomeWorkITVDN_OOP_L1_p1/Program.cs
+++ KSergey.HomeWorkITVDN_OOP_L1_p1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,29 @@ namespace KSergey.HomeWorkITVDN_OOP_L1_p1
     {
         static void Main(string[] args)
         {
+            //вводим с клавы значение сторон
+            Console.WriteLine("Введите значение первой стороны прямоугольника");
+
+            //UNDONE узнать у Вадима как правильно использовать данную конструкцию
+            /* как правильно это записывать не полуячилось???
+             * if (int.tryparce(Console.ReadLine(), out int input))
+            {
+                return side1;
+            }
+            */
+            
+            double side1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение второй стороны прямоугольника");
+            double side2 = Convert.ToInt32(Console.ReadLine());
+
+            // инициализирую новый экземпляр класса myRectangle
+            Rectangle myRectangle = new Rectangle(side1, side2);
+
+            // вывожу значение площади и периметра
+            Console.WriteLine($"Площадь прямоугольника = {myRectangle.Area}. Периметр прямоугольника = {myRectangle.Perimeter}");
+
+            //задержка консоли
+            Console.ReadKey();
         }
     }
 }
--- KSergey.HomeWorkITVDN_OOP_L1_p1/Rectangle.cs
+++ KSergey.HomeWorkITVDN_OOP_L1_p1/Rectangle.cs
@@ -8,9 +8,11 @@ namespace KSergey.HomeWorkITVDN_OOP_L1_p1
 {
     class Rectangle
     {
+        // создаю входящие поля
         private double side1;
         private double side2;
 
+        // создаю конструктор и  прописываю значения полей по умолчанию
         public Rectangle(double side1, double side2)
             {
 
@@ -20,18 +22,32 @@ namespace KSergey.HomeWorkITVDN_OOP_L1_p1
 
             }
 
-        public double AreaCalculator(double side1, double side2)
+        // метод расчитывающий прощадь прямоугольника
+        double AreaCalculator()
         {
             return side1*side2;
         }
 
+        // свойство только для чтения возвращает значение площади
         public double Area
         { get
             {
-                return Area;
+                return this.AreaCalculator();
             }
         }
+        // метод расчитывающий периметр прямоугольника
+        double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
 
-
+        // свойство только для чтения возращает значение периметра
+        public double Perimeter
+        {
+            get
+            {
+                return this.PerimeterCalculator();
+            }
+        }
     }
 }
