418c599fbd33042c08d7a7ff878f1ffea05d22cb
Sergey <krivolapovs@mail.ru>
2020-07-18 07:31:18 +0300

    Добавьте файлы проекта.

---
+++ KSergey.HomeWorkITVDN_OOP_L1_p1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSergey.HomeWorkITVDN_OOP_L1_p1
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ KSergey.HomeWorkITVDN_OOP_L1_p1/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSergey.HomeWorkITVDN_OOP_L1_p1
+{
+    class Rectangle
+    {
+        private double side1;
+        private double side2;
+
+        public Rectangle(double side1, double side2)
+            {
+
+            this.side1 = side1;
+            this.side2 = side2;
+
+
+            }
+
+        public double AreaCalculator(double side1, double side2)
+        {
+            return side1*side2;
+        }
+
+        public double Area
+        { get
+            {
+                return Area;
+            }
+        }
+
+
+    }
+}
